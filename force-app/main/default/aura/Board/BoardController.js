18248130c161838389671c8431ee47fde13334fc
Vivek <vivekwithkumar@gmail.com>
2022-03-14 03:41:17 +0530

    contains block components and the main part of the game

---
+++ force-app/main/default/aura/Board/BoardController.js
@@ -0,0 +1,46 @@
+({
+    doInit : function(component, event, helper) {
+        console.log('Initialization complete');
+        let gameMode = component.get('v.mode');
+        let column = 0;
+        if(gameMode && gameMode === 'hard'){
+            column = 6;
+        }
+        else if(gameMode && gameMode === 'medium'){
+            column = 4;
+        }
+        else{
+            column = 3;
+        }
+
+        let blockSize = 12/column;
+        component.set('v.blockSize', blockSize);
+
+        // build a list of 100 words and get random words out of it
+        let words = helper.getWords(column * column);
+        
+        // get win word
+        let winWord = helper.getWinWord(words);
+        
+        component.set('v.words', words);
+        component.set('v.winWord', winWord);
+    },
+    doRender : function(component, event, helper){
+        console.log('Render completed');
+    },
+    blockClickHandler : function(component, event, helper){
+        let clickCount = component.get('v.clickCount') + 1;
+        let value = event.getParam("value");
+        if(value === component.get('v.winWord')){
+            // user has won
+            component.set('v.result', 'YOU WIN');
+            console.log('You win');
+        }
+        else if(clickCount === 3){
+            // user has lost
+            component.set('v.result', 'YOU LOSE');
+            console.log('You lose');
+        }
+        component.set('v.clickCount', clickCount);
+    }
+})
---
+++ force-app/main/default/aura/Board/BoardHelper.js
@@ -0,0 +1,37 @@
+({
+    getWords : function(count) {
+        if(count > 100)return;
+        let wordsArray = ['ukicxka', 'fomfvac', 'wkfpzhk', 'aswfvud', 'lltbsfk', 'qghemwc', 
+                          'vqdevqq', 'zmchzjm', 'khybsly', 'xeftaua', 'khqenxt', 'vxgvsgn', 
+                          'jlvzmwj', 'lzcqnto', 'ofwmcqx', 'vuwapoz', 'hdfvmld', 'sjodbsi', 
+                          'txmqzta', 'fqaugae', 'ogwwkfm', 'iqnswcv', 'wdwreye', 'nydnrrh', 
+                          'lxhqlrg', 'nqepgyu', 'jsehhdp', 'ymspsri', 'dbpwjcp', 'iiakpdt', 
+                          'ajsaltm', 'bikusti', 'whhzhay', 'jjuxpzf', 'ltsooxp', 'cpfesnb', 
+                          'rqbjuix', 'tpyneuu', 'xkkngko', 'ttvuleg', 'edssxlt', 'fupcaep', 
+                          'xcvpiek', 'ifbvlcr', 'qvvefzy', 'mvvkmlt', 'sshryki', 'ohsgvtx', 
+                          'rcyaknr', 'wimkixu', 'tegszvz', 'aytwenl', 'rrusocw', 'fzaledo', 
+                          'etrsclj', 'sltloga', 'ifiazuy', 'owufkps', 'wuphhuo', 'kgqdhwa', 
+                          'xqketdh', 'yqxhgfw', 'josecul', 'isoonwz', 'irkosdn', 'pkkbszn', 
+                          'nxxqeec', 'giujwtt', 'usqdvhf', 'igxdpzm', 'mlhqzzg', 'erooktf', 
+                          'wjhlzev', 'gyhpinh', 'rhgdwtj', 'vjajzel', 'xfeowrw', 'nasoecg', 
+                          'wctltmu', 'pojsezt', 'weotxyv', 'fboibqn', 'xmseoeb', 'xdewxxr', 
+                          'ytfsmeq', 'stqvggz', 'klnjswt', 'ekfkopw', 'gmzyvbo', 'qzynikl', 
+                          'miybrke', 'mzzxwex', 'qljgvkx', 'sbatkcf', 'xybrrtw', 'lvkpkio', 
+                          'fuuxbwy', 'lnzusfq', 'mimmkff', 'ypqmkyw'];
+        // Randomize Array
+        this.randomizeArray(wordsArray);
+        return wordsArray.slice(0, count);
+    },
+    randomizeArray : function (arr) {
+        for (var i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    },
+    getWinWord : function(arr){
+        const randomWord = arr[Math.floor(Math.random()*arr.length)];
+        return randomWord;
+    }
+})
